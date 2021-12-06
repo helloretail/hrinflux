e0281b6e1bc053ed537108ebf3583b5507b26c15
Bjørn Hartfelt <bha@helloretail.com>
2021-12-06 15:13:36 +0100

    Add types.

--- hrinflux/influx.py
+++ hrinflux/influx.py
@@ -1,12 +1,14 @@
 import socket
 import time
+from types import TracebackType
+from typing import Any, Dict, Optional, Type
 
 class Influx(object):
-	def __init__(self, host='influxdb.addwish.com', port=4444):
+	def __init__(self, host: str = 'influxdb.addwish.com', port: int = 4444):
 		self.addr = host, int(port)
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	
-	def send(self, name, value, **args):
+	def send(self, name: str, value: Any, **args: Dict[str, str]) -> None:
 		self.sock.sendto(
 			("{}{} value={}".format(
 				name,
@@ -14,19 +16,22 @@ class Influx(object):
 				str(value))).encode(),
 			self.addr)
 	
-	def time(self, metric, **args):
+	def time(self, metric: str, **args: Dict[str, str]) -> '_Timed':
 		return _Timed(self, metric, **args)
 
 class _Timed(object):
-	def __init__(self, influx, metric, **args):
+	def __init__(self, influx: Influx, metric: str, **args: Dict[str, str]):
 		self.influx = influx
 		self.metric = metric
 		self.args = args
 	
-	def __enter__(self):
+	def __enter__(self) -> None:
 		self.start = time.perf_counter()
 	
-	def __exit__(self, exc_type, exc_val, exc_tb):
+	def __exit__(self,
+			exc_type: Optional[Type[BaseException]],
+			exc_val: Optional[BaseException],
+			exc_tb: Optional[TracebackType]) -> None:
 		self.influx.send(
 			self.metric,
 			time.perf_counter() - self.start,
